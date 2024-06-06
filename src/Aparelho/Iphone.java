13f2ef8a690e7029200177bc4af128c1f7f715c3
Silenoz <joao.vale456@gmail.com>
2024-06-06 20:53:22 -0300

    teste

--- src/Aparelho/Iphone.java
+++ src/Aparelho/Iphone.java
@@ -1,2 +1,55 @@
-public class Iphone {
+package Aparelho;
+
+import IphoneService.AparelhoTelefonico;
+import IphoneService.NavegadorInternet;
+import IphoneService.ReprodutorMusical;
+
+public class Iphone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+    @Override
+    public void ligar(String numero) {
+        System.out.println("Ligando para " + numero + "...");
+    }
+
+    @Override
+    public void atender() {
+        System.out.println("Atendendo ligação...");
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        System.out.println("Iniciando correio de voz...");
+    }
+
+    @Override
+    public void exibirPagina(String url) {
+        System.out.println("Exibindo página: " + url);
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        System.out.println("Adicionando nova aba...");
+    }
+
+    @Override
+    public void atualizarPagina() {
+        System.out.println("Atualizando página...");
+
+    }
+
+    @Override
+    public void tocar() {
+        System.out.println("Reproduzindo música...");
+
+    }
+
+    @Override
+    public void pausar() {
+        System.out.println("Pausando música..");
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+        System.out.println("Selecionando música: " + musica);
+
+    }
 }
